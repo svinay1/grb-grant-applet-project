efc532c9012f7feb7948d6e87b84afc50439bf05
Samhita Vinay <samhitavinay@gmail.com>
2025-10-23 16:51:33 -0500

    Finished logistic regression

--- scripts/plot4.js
+++ scripts/plot4.js
@@ -1,41 +1,44 @@
 function generateUniformData(totalPoints) {
     const rand = d3.randomLcg(42);
-    const x = [];
-    const y = [];
     const data = [];
 
     for (let i = 0; i < totalPoints; i++) {
         if (i < totalPoints / 2) {
-            x.push((rand() - 0.5) * 4 - 2);
-            y.push(0);
+            const x = (rand() - 0.5) * 4 - 2;
+            data.push({x: x, y: 0}); 
         } else {
-            x.push((rand() - 0.5) * 4 + 0.5);
-            y.push(1);
-        }
-
-        const pred = (100 / (1 + Math.E ** -(3.2 * x[i] + 3.1))) > 0.5;
-
-        if (y[i] == 0) { 
-            if (pred == 0) {
-                data.push({x: x[i], y: y[i], marker: 'o', color: 'blue'})
-            } else { 
-                data.push({x: x[i], y: y[i], marker: 'x', color: 'blue'})
-            }
-        } else { 
-            if (pred == 0) { 
-                data.push({x: x[i], y: y[i], marker: 'x', color: 'orange'})
-            } else { 
-                data.push({x: x[i], y: y[i], marker: 'o', color: 'orange'})
-            }
+            const x = (rand() - 0.5) * 4 + 0.5;
+            data.push({x: x, y: 100}); 
         }
     }
-    return data;
+    return data; // Return only the data
+}
+
+function getAccuracy(predictY, initialData) {
+    let correctCount = 0;
+    initialData.forEach(d => {
+        const pred_prob = predictY(d.x);
+        const pred_class = pred_prob > 50; 
+        const true_class = d.y > 50;      
+
+        if (pred_class === true_class) {
+            correctCount++;
+            d.marker = 'o'; 
+        } else {
+            d.marker = 'x'; 
+        }
+        d.color = (true_class) ? 'orange' : 'blue'; 
+    });
+    
+    const accuracy = (correctCount / initialData.length) * 100;
+    return { data: initialData, accuracy: accuracy }
 }
 
 function plot4() {
     // Colors used in plot
     let color_data = "#3070B7";
     let color_grid = "#d9c7d7";
+    let color_estimate = "#377e22";
 
     // Font
     let font = 'Arial';
@@ -51,18 +54,21 @@ function plot4() {
     // Append the svg object to the body of the page
     var svg = appendSvg("#myplot", width, height, margin);
             
-    // Get the data
-    const data = generateUniformData(50);
+    // Get the data and accuracy
+    let initialData = generateUniformData(50);
 
     // Get m and b from variables drawn from text fields
     let m = parseFloat(document.getElementById('mBox').value);
     let b = parseFloat(document.getElementById('bBox').value);
 
     // Set the function.
-    const predictPolynomial = x => c2 * (x ** 2) + c1 * x + b;
+    const predictY = x => 100 / (1 + Math.exp(-(m * x + b)));
+
+    // Get finalized data and accuracy.
+    let { data, accuracy } = getAccuracy(predictY, initialData);
 
     // Min and max x and y
-    let xmin = -5, xmax = 5, ymin = 0, ymax = 1;
+    let xmin = -5, xmax = 5, ymin = 0, ymax = 100;
 
     // Add x axis
     let x = addXAxis(svg, xmin, xmax, width, height);
@@ -75,13 +81,13 @@ function plot4() {
     drawGridlines(7, 9, width, height, color_grid);
 
     // Draw line for estimate
-    //drawEstimateLine(svg, x, y, xmin, xmax, predictPolynomial, color_estimate);
+    drawEstimateLine(svg, x, y, xmin, xmax, predictY, color_estimate);
 
     // Add dots
     addDots(svg, data, x, y, color_data);
     
     // Title
-    addTitle(`Logistic Regression with 1 Covariate: \\( \y = \\frac{100}{1 + e^{-(mx + b)}} \\), `);
+    addTitle(`Doing Logistic Regression Manually: accuracy = ${accuracy.toFixed(1)}%`);
 
     // y label
     addYLabel(svg, font, height, -margin.left / 2, "Probability that y=1");
@@ -95,28 +101,15 @@ function plot4() {
     // Legend entries
     addLegendEntry("y=1; pred=1", "circle", 'orange', 150, -400);
     addLegendEntry("y=1; pred=0", "x", 'orange', 150, -400);
-    addLegendEntry("y=0; pred=1", "circle", 'blue', 150, -400);
     addLegendEntry("y=0; pred=0", "x", 'blue', 150, -400);
+    addLegendEntry("y=0; pred=1", "circle", 'blue', 150, -400);
 
     // Find the solution to the regression
     function solveRegression() {
-        // Extract X and y matrices
-        const X = data.map(d => [1, d.x, d.x ** 2]);
-        const y = data.map(d => d.y);
-
-        // Matrix multiplication
-        const X_t = math.transpose(X);
-        const X_tX = math.multiply(X_t, X);
-        const coefs = math.multiply(math.inv(X_tX), math.multiply(X_t, y));
-
-        // Extract coefficients and assign them to the box and sliders
-        const b = +coefs[0].toFixed(4);
-        const m = +coefs[1].toFixed(4);
-
-        document.getElementById('bBox').value = b;
-        document.getElementById('bSlider').value = b;
-        document.getElementById('mBox').value = m;
-        document.getElementById('mSlider').value = m;
+        document.getElementById('bSlider').value = 0.3;
+        document.getElementById('bBox').value = 0.3;
+        document.getElementById('mSlider').value = 1.7;
+        document.getElementById('mBox').value = 1.7;
     }
 
     window.solveRegression = solveRegression;
